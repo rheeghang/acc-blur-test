241c7109bba9cf3919c7d68c2d55d27153024571
“rheeghang <ryujh21h@pati.kr>
2025-04-09 17:42:06 +0900

    각도모드 블랙

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -57,7 +57,7 @@ const Menu = ({ isOpen, onClose, onPageSelect, pageNumber, pageType }) => {
 
         <div className="px-4 py-2 border-b border-gray-200">
           <div className="flex items-center justify-between">
-            <span className="text-base">
+            <span className="text-black">
               {language === 'ko' 
                 ? (isOrientationMode ? '각도 모드' : '각도해제 모드')
                 : (isOrientationMode ? 'Angle Mode' : 'Angle Off')
